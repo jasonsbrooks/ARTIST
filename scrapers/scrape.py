4cdd3b8c2569a78ba057390c44feffd367de78e1
Charlie Proctor <charlie@charlieproctor.com>
2016-02-27 23:45:41 -0500

    convert to generic, multithreaded scraper

---
+++ scrapers/scrape.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+"""
+Jazz MIDI scraper
+USAGE: scrape.py [SITE_JSON]
+"""
+import urllib,urllib2,os,sys,json,threading
+from urlparse import urljoin
+from BeautifulSoup import BeautifulSoup
+
+# class for site requests (a thread)
+class SiteRequest(threading.Thread):
+    
+    def __init__(self, sitename, base_url):
+        threading.Thread.__init__(self)
+        self.sitename = sitename
+        self.base_url = base_url
+
+    def run(self):
+		# request the "homepage"
+		req = urllib2.Request(self.base_url)
+		response = urllib2.urlopen(req)
+		page_contents = response.read()
+
+		# grab all the MIDI links
+		for link in BeautifulSoup(page_contents).findAll('a',href=True):
+			if link['href'].endswith('.mid'):
+				base_url = urljoin(self.base_url,link['href'])
+				print "Scraping " + base_url
+				
+				try:
+					urllib.URLopener().retrieve(base_url, self.sitename 
+						+ "/" + link['href'].split('/')[-1])
+				except IOError,e:
+					print e
+
+if __name__ == '__main__':
+	# open the data file
+	if len(sys.argv) > 1:
+		sites_filename = sys.argv[1]
+	else:
+		sites_filename = "sites.json"
+
+	with open(sites_filename) as sites_file:    
+	    sites = json.load(sites_file)['sites']
+
+	# iterate through all the sites
+	for site in sites:
+		sitename = site['name']
+
+		# and all the urls for this site
+		for base_url in site['urls']:
+	    	# make directory as necessary
+			if not os.path.exists(sitename):
+				os.makedirs(sitename)
+
+			# and start the requests
+			t = SiteRequest(sitename,base_url)
+			t.start()
+					
