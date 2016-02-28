bb7bd855af87e3e163a914b1206d695b305594c1
Kevin Jiang <k.jiang@yale.edu>
2016-02-28 16:54:24 -0500

    added note transposer code for playback in limited pitch range

--- helpers.py
+++ helpers.py
@@ -1,26 +1,28 @@
 import midi
+import sys
 
 
+# takes midi file and constructs dictionary with following structure:
+# {instr_key: [track1, track2, ...], ...} where track1, track2...are list of tuples returned by get_notes
 # currently assumes there exists only one ProgramChangeEvent per instrument track
-# also assumes that ProgramChangeEvent represents
 def extract(midifilename):
-    instr_notes_dic = {}  # {instr_key: [track1, track2]} where track1...are list of NoteOnEvents and NoteOffEvents
+    instr_tracks_dic = {}  # 
     pattern = midi.read_midifile(midifilename)
 
     for track in pattern:
-        print track[0:20]
+        # print track[0:20]
         for event in track:
             if type(event) is midi.ProgramChangeEvent:  # only act if this track is an actual instrument track!
                 instr_key = event.data[0]  # event.data[0] contains instrument key
                 notes = get_notes(track)
 
-                if not(instr_key in instr_notes_dic):
-                    instr_notes_dic[instr_key] = [notes]
+                if not(instr_key in instr_tracks_dic):
+                    instr_tracks_dic[instr_key] = [notes]
                 else:
-                    instr_notes_dic[instr_key].append(notes)
+                    instr_tracks_dic[instr_key].append(notes)
 
                 break  # this assumes that there is only one ProgramChangeEvent per instrument track
-    return instr_notes_dic
+    return instr_tracks_dic
 
 
 # given a track returns NoteOnEvent and NoteOffEvent as list of tuples (on/off, tick, pitch, velocity)
@@ -37,8 +39,67 @@ def get_notes(track):
     return notes
 
 
-# test midi
-# test_dic = extract("MIDI_sample.mid")
-# for k,v in test_dic.iteritems():
-#     print k
-#     print v
+# transposes pitch to be same pitch within range [lo,hi] inclusive
+# if multiple pitch matches exist within range, match closest to given pitch
+def transpose_pitch(pitch, lo, hi):
+    octave = 12
+
+    if (hi - lo) < (octave - 1):
+        print "Transpose Error: hi - lo < 11, make sure range spans a full octave"
+        sys.exit(1)
+
+    while pitch < lo:
+        pitch += octave
+    while pitch > hi:
+        pitch -= octave
+
+    return pitch
+
+
+# transposes list of notes in structure returned by function get_notes
+# within range [lo, hi] inclusive
+def transpose_note_list(note_list, lo, hi):
+    new_notes = []
+
+    for note in note_list:
+        new_pitch = transpose_pitch(note[2], lo, hi)
+        new_note = (note[0], note[1], new_pitch, note[2])
+        new_notes.append(new_note)
+
+    return new_notes
+
+
+# transposes all notes within dictionary returned by function extract
+# within range [lo, hi] inclusive
+def transpose_instr_tracks_dic(instr_tracks_dic, lo, hi):
+    new_instr_tracks_dic = {}
+
+    for instr_key, list_of_tracks in instr_tracks_dic.iteritems():
+        for track in list_of_tracks:
+            new_note_list = transpose_note_list(track, lo, hi)
+
+            if not(instr_key in new_instr_tracks_dic):
+                new_instr_tracks_dic[instr_key] = [new_note_list]
+            else:
+                new_instr_tracks_dic[instr_key].append(new_note_list)
+
+    return new_instr_tracks_dic
+
+
+# test helper methods
+def main():
+    test_dic = extract("MIDI_sample.mid")
+    test_note_list = []
+
+    for k, v in test_dic.iteritems():
+        # print repr(k)
+        test_note_list = v[0]
+        # print len(v)
+
+    print transpose_note_list(test_note_list, 40, 51)
+    print transpose_instr_tracks_dic(test_dic, 40, 51)
+
+
+if __name__ == "__main__":
+    # main()
+    pass
