33c9c820c58944235f0d29bdf0969140d2e4ce9c
Charlie Proctor <charlie@charlieproctor.com>
2016-04-03 17:27:03 -0400

    reordering imports in __init__.py

--- non_baxter/db/__init__.py
+++ non_baxter/db/__init__.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from song import Song
-from track import Track
-from note import Note
-
 engine = create_engine('sqlite:////tmp/artist.db')
 Session = sessionmaker(bind=engine)
 
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
+
+from song import Song
+from track import Track
+from note import Note
