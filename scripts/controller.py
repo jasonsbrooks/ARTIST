a554048bfc599d7fcca88d49cfe86a0d9f65adc8
Charlie Proctor <charlie@charlieproctor.com>
2016-04-17 14:38:35 -0400

    superclass BaxterController in controller.py

---
+++ scripts/controller.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import rospy, baxter_interface, json
+from baxter_interface import CHECK_VERSION
+
+class BaxterController(object):
+    def __init__(self):
+        """
+        Initialize control with Baxter
+        """
+        # verify robot is enabled
+        rospy.loginfo("Getting robot state... ")
+        self._rs = baxter_interface.RobotEnable(CHECK_VERSION)
+        self._init_state = self._rs.state().enabled
+        rospy.loginfo("Enabling robot... ")
+        self._rs.enable()
+        rospy.on_shutdown(self.clean_shutdown)
+        rospy.loginfo("Running...")
+
+        # Get the body running
+        self.head = baxter_interface.Head()
+        self.right_arm = baxter_interface.Limb('right')
+        self.left_arm = baxter_interface.Limb('left')
+        self.left_gripper = baxter_interface.Gripper('left', CHECK_VERSION)
+        self.right_gripper = baxter_interface.Gripper('right', CHECK_VERSION)
+        self.left_navigator = baxter_interface.Navigator('left')
+        self.right_navigator = baxter_interface.Navigator('right')
+        self.left_torso_navigator = baxter_interface.Navigator('torso_left')
+        self.right_torso_navigator = baxter_interface.Navigator('torso_right')
+
+    def clean_shutdown(self):
+        """
+        Exits example cleanly by disabling robot
+        """
+        rospy.loginfo("Exiting example...")
+
+        if not self._init_state and self._rs.state().enabled:
+            rospy.loginfo("Disabling robot...")
+            self._rs.disable()
+
+    def set_neutral(self,on=True):
+        """
+        Move both arms to the neutral position
+        """
+        if not on:
+            return False
+
+        rospy.loginfo("[set_neutral] Moving arms to neutral position.")
+
+        # load the config file
+    	with open("./src/baxter_artist/scripts/conf.json") as f:
+		    CONFIG = json.load(f)
+
+        rospy.loginfo("[set_neutral] CONFIG: %s",CONFIG)
+
+        # and move to neutral position
+        self.left_arm.move_to_joint_positions(CONFIG["neutral"]["left"])
+        self.right_arm.move_to_joint_positions(CONFIG["neutral"]["right"])
+
+        rospy.loginfo("[set_neutral] Both arms in neutral position.")
+
+def main():
+    rospy.loginfo("Initializing node... ")
+    rospy.init_node("play_xylophone")
+    controller = BaxterController()
+
+    rospy.on_shutdown(controller.clean_shutdown)
+    controller.set_neutral()
+
+    rospy.signal_shutdown("Finished control")
+
+if __name__ == '__main__':
+    main()
--- scripts/learn_keys.py
+++ scripts/learn_keys.py
@@ -4,6 +4,8 @@ import baxter_interface
 from baxter_interface import CHECK_VERSION
 import rospy, pdb, os, json, time, threading
 
+from controller import BaxterController
+
 import cv2, cv_bridge
 
 from sensor_msgs.msg import (
@@ -16,27 +18,10 @@ NEUTRAL_KEY = 0
 with open("./src/baxter_artist/scripts/conf.json") as f:
     CONFIG = json.load(f)
 
-class Learner(object):
+class Learner(BaxterController):
     def __init__(self):
 
-        # verify robot is enabled
-        rospy.loginfo("Getting robot state... ")
-        self._rs = baxter_interface.RobotEnable(CHECK_VERSION)
-        self._init_state = self._rs.state().enabled
-        rospy.loginfo("Enabling robot... ")
-        self._rs.enable()
-        rospy.loginfo("Running. Ctrl-D to quit")
-
-        # Get the body running
-        self.head = baxter_interface.Head()
-        self.right_arm = baxter_interface.Limb('right')
-        self.left_arm = baxter_interface.Limb('left')
-        self.left_gripper = baxter_interface.Gripper('left', CHECK_VERSION)
-        self.right_gripper = baxter_interface.Gripper('right', CHECK_VERSION)
-        self.left_navigator = baxter_interface.Navigator('left')
-        self.right_navigator = baxter_interface.Navigator('right')
-        self.left_torso_navigator = baxter_interface.Navigator('torso_left')
-        self.right_torso_navigator = baxter_interface.Navigator('torso_right')
+        BaxterController.__init__(self)
 
         # initialize right_note, left_note
         self.left_note = NEUTRAL_KEY
@@ -72,26 +57,9 @@ class Learner(object):
         self.right_navigator._outer_led.set_output(False)
 
     def clean_shutdown(self):
-        """
-        Exits example cleanly by moving head to neutral position and
-        maintaining start state
-        """
-        rospy.loginfo("\nExiting example...")
 
         self.keys.write()
-
-        if not self._init_state and self._rs.state().enabled:
-            rospy.loginfo("Disabling robot...")
-            self._rs.disable()
-
-    def set_neutral(self,on=True):
-        if not on:
-            return False
-        
-        self.left_arm.move_to_joint_positions(CONFIG["neutral"]["left"])
-        self.right_arm.move_to_joint_positions(CONFIG["neutral"]["right"])
-
-        rospy.loginfo("[set_neutral] CONFIG: %s",CONFIG)
+        BaxterController.clean_shutdown(self)
 
     def get_joint_angles(self):
         return self.left_arm.joint_angles(), self.right_arm.joint_angles()
@@ -226,7 +194,6 @@ def main():
     rospy.loginfo("Initializing node... ")
     rospy.init_node("play_xylophone",log_level=rospy.DEBUG)
     learner = Learner()
-    rospy.on_shutdown(learner.clean_shutdown)
 
     learner.shell.start()
     learner.shell.join()
