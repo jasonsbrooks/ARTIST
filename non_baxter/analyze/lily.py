cfe7fb7ba3b926ac23195c585c7d55a7807daf36
Charlie Proctor <charlie@charlieproctor.com>
2016-04-20 04:37:41 -0400

    debugging lily.py

--- non_baxter/analyze/lily.py
+++ non_baxter/analyze/lily.py
@@ -8,10 +8,34 @@ lpc.context = lpMusicList
 
 session = Session()
 
-for note in session.query(Track).first().notes:
-    if note.dur == 0:
-        continue
-    m_note = music21.note.Note(note.iso_pitch,quarterLength=note.dur/8.0)
-    lpc.appendContextFromNoteOrRest(m_note)
+chord = []
 
-lpc.showPDF()
+for note in session.query(Track).first().notes:
+
+    if len(chord) == 0:
+        chord.append(note)
+    elif note.start == chord[-1].start:
+        chord.append(note)
+    else:
+        ql = note.dur / 8.0
+        if ql < 0.25:
+            continue
+        ql = round(ql,1)
+
+        try:
+            ch = music21.chord.Chord([n.iso_pitch for n in chord],quarterLength=ql)
+        except Exception,e:
+            chord = []
+            continue
+
+        chord = []
+
+        try:
+            lpc.appendM21ObjectToContext(ch)
+            print ch
+        except Exception,e:
+            continue
+
+# lpc.writeLyFile(ext="ly",fp="out.ly")
+with open("out.ly","w") as outfile:
+    outfile.write(str(lpMusicList))
