ee15f6e7657adf29bc18a83c087390fef9a92923
Charlie Proctor <charlie@charlieproctor.com>
2016-04-20 03:38:21 -0400

    better model parameters

--- non_baxter/analyze/chords.py
+++ non_baxter/analyze/chords.py
@@ -17,7 +17,7 @@ def compatibility(m_root,m_note):
     except ValueError,e:
         return 0
 
-# a note is a strong beat if it starts the measure
+# the strength of the note corresponds to where it lies relative to 1,1/2,1/4 notes...
 def beat_strength(note):
     if note.start % 32 == 0:
         return 3
@@ -30,13 +30,24 @@ def beat_strength(note):
 
 line_of_fifths = ["B#","E#","A#","D#","G#","C#","F#","B","E","A","D","G","C","F","B-","E-","A-","D-","G-","C-","F-"]
 
+# return the difference between two notes on the line of fifths.
 def lof_difference(m_prev,m_note):
     prev_pos = line_of_fifths.index(m_prev.name)
     note_pos = line_of_fifths.index(m_note.name)
 
     return abs(note_pos - prev_pos)
 
-def best_next_root(m_prev,m_note):
+# PARAMETERS TO THE MODEL:
+COMPATIBILITY_MULTIPLIER = 1
+LOF_MULTIPLIER = -1
+STRENGTH_MULTIPLIER = 1
+
+THRESHOLD_MULTIPLIER = 0.9
+
+def best_next_root(m_prev,chord):
+
+    # calculate the beat strength
+    stren = beat_strength(chord[0])
 
     # start with C, weight of 0
     best_root,best_weight = music21.note.Note('C'),-len(line_of_fifths)
@@ -44,48 +55,61 @@ def best_next_root(m_prev,m_note):
     # try all possible roots
     for m_root in music21.scale.ChromaticScale('C').pitches:
 
-        # compatibility score
-        comp = compatibility(m_root,m_note)
+        # compatibility scores
+        comp = []
+        for note in chord:
+            m_note = music21.note.Note(note.iso_pitch)
+            comp.append(compatibility(m_root,m_note))
+        comp_score = sum(comp) / float(len(chord))
 
         # difference from previous chord root on line of fifths
-        lof = (lof_difference(m_prev,m_note) if m_prev else 0)
+        lof = (lof_difference(m_prev,m_root) if m_prev else 0)
 
-        val = comp - lof
+        val = STRENGTH_MULTIPLIER * stren + COMPATIBILITY_MULTIPLIER * comp_score + LOF_MULTIPLIER * lof
 
         if val > best_weight:
             best_root,best_weight = m_root,val
 
     return best_root,best_weight
 
+def main():
+    chord = []
 
-for trk in session.query(Track).all():
+    for trk in session.query(Track).all():
 
-    # previous note
-    m_prev = None
+        # previous note
+        m_prev = None
 
-    # transition threshold
-    threshold = -len(line_of_fifths)
+        # transition threshold
+        threshold = -len(line_of_fifths)
 
-    for note in trk.notes:
-        m_note = music21.note.Note(note.iso_pitch)
+        # iterate through all notes in this track
+        for note in sorted(trk.notes,key=lambda note: note.start):
 
-        note.root = (m_prev.midi if m_prev else 0)
-        note.iso_root = (m_prev.name if m_prev else "")
+            # initialize root
+            note.root = m_prev.midi if m_prev else 0
+            note.iso_root = m_prev.name if m_prev else ""
 
-        # when starts on quarter note
-        if beat_strength(note) > 0:
-            (best_root,best_weight) = best_next_root(m_prev,m_note)
-
-            if best_weight > threshold:
-                # use this as the new root!
-                print best_root
-
-                note.root = best_root.midi
-                note.iso_root = best_root.name
-
-                m_prev = best_root
+            if len(chord) == 0:
+                chord.append(note)
+            elif note.start == chord[-1].start:
+                chord.append(note)
             else:
-                threshold *= 0.9
+                #     process the chord
+                best_root,best_weight = best_next_root(m_prev,chord)
 
-    session.commit()
+                # update node.root if better than threshold
+                if best_weight > threshold:
+                    note.root = best_root.midi
+                    note.iso_root = best_root.name
+                    m_prev,threshold = best_root,best_weight
+                    print note.iso_root
+                else:
+                    threshold *= THRESHOLD_MULTIPLIER
 
+                chord = []
+
+        session.commit()
+
+if __name__ == '__main__':
+    main()
