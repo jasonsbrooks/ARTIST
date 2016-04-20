5ae80b084b7d32f79338344c4daa21a4549fccb7
Kevin Jiang <k.jiang@yale.edu>
2016-04-20 01:15:37 -0400

    full barebones ga complete

--- non_baxter/ga/ga.py
+++ non_baxter/ga/ga.py
@@ -178,14 +178,134 @@ def crossover(parent1, parent2, d):
 
     return (new_genotype1, new_genotype2)
 
+# transposes pitch to be same pitch within range [lo,hi] inclusive
+# if multiple pitch matches exist within range, match closest to given pitch
+def transpose_pitch(pitch, lo, hi):
+    octave = 12
 
-def local_mutation(chromosome):
-    
+    if (hi - lo) < (octave - 1):
+        print "Transpose Error: hi - lo < 11, make sure range spans a full octave"
+        sys.exit(1)
+
+    while pitch < lo:
+        pitch += octave
+    while pitch > hi:
+        pitch -= octave
+
+    return pitch
+
+# transpose fragment (ed, dur) by random number of degrees
+def mut1(genotype, start_index, end_index):
+    transpose = random.randint(-5, 5)
+    for i, (ed, dur) in enumerate(genotype):
+        if i >= start_index and i <= end_index:
+            genotype[i] = (transpose_pitch(genotype[i][0] + transpose, 1, 21), genotype[i][1])
+
+    return genotype
+
+
+# permute in time
+def mut2(genotype, start_index, end_index):
+    fragment = genotype[start_index:end_index+1]
+    random.shuffle(fragment)
+    for i, (ed, dur) in enumerate(genotype):
+        if i >= start_index and i <= end_index:
+            genotype[i] = fragment[i-start_index]
+
+    return genotype
+
+
+# sort into ascending or descending pitch
+def mut3(genotype, start_index, end_index):
+    fragment = genotype[start_index:end_index+1]
+    # 50% chance ascneding/descending
+    if random.random() < .5:
+        fragment.sort(key=lambda x: x[0], reverse=False)
+    else:
+        fragment.sort(key=lambda x: x[0], reverse=True)
+
+    for i, (ed, dur) in enumerate(genotype):
+        if i >= start_index and i <= end_index:
+            genotype[i] = fragment[i-start_index]
+
+    return genotype
+
+
+# reverse in time
+def mut4(genotype, start_index, end_index):
+    fragment = genotype[start_index:end_index+1]
+    # 50% chance ascneding/descending
+    fragment.reverse()
+
+    for i, (ed, dur) in enumerate(genotype):
+        if i >= start_index and i <= end_index:
+            genotype[i] = fragment[i-start_index]
+
+    return genotype
+
+# change few pitches while maintaining same rhythm
+def mut5(genotype, start_index, end_index):
+    fragment = genotype[start_index:end_index+1]
+
+    for i, (ed, dur) in enumerate(genotype):
+        if i >= start_index and i <= end_index:
+            if random.random() < .3:
+                genotype[i] = (random.randint(1, 21), genotype[i][1])
+
+    return genotype
+
+# one-note mutation which changes pitch of one note up or down
+def mut6(genotype, start_index):
+
+    for i, (ed, dur) in enumerate(genotype):
+        if i == start_index:
+            if random.random() < .5:
+                genotype[i] = (transpose_pitch(1 + genotype[i][0], 1, 21), genotype[i][1])
+            else:
+                genotype[i] = (transpose_pitch(-1 + genotype[i][0], 1, 21), genotype[i][1])
+
+
+
+    return genotype
+
+
+# mutates in one of many ways locally
+# returns new mutated genotype
+def local_mutation(chromosome, d, prob_local=.1):
+    genotype = chromosome[1]
+    len_genotype = len(genotype)
+    end_index = random.randint(0, len_genotype-1)
+    start_index = random.randint(0, len_genotype-1)
+    if start_index > end_index:
+        temp = end_index
+        end_index = start_index
+        random_end_index = temp
+
+
+    if random.random() < prob_local:
+        genotype = mut1(genotype, start_index, end_index)
+
+    if random.random() < prob_local:
+        genotype = mut2(genotype, start_index, end_index)
+
+    if random.random() < prob_local:
+        genotype = mut3(genotype, start_index, end_index)
+
+    if random.random() < prob_local:
+        genotype = mut4(genotype, start_index, end_index)
+
+    if random.random() < prob_local:
+        genotype = mut5(genotype, start_index, end_index)
+
+    if random.random() < prob_local:  # one note mutation
+        genotype = mut6(genotype, start_index)
+
+    return genotype
 
 
 # returns mutated chromosome
-def mutate(chromosome):
-    chromosome = local_mutation(chromosome)
+def mutate(chromosome, d, prob_local=.1):
+    chromosome = local_mutation(chromosome, d, prob_local)
     return chromosome
 # runs ga on
 # population of size n
@@ -198,6 +318,7 @@ def ga(n, chord_progression, num_iter):
         # Elitism?
         # keep the highest fitness chromosome
         chromosome_list.sort(key=lambda x: x[0])  # sort by increasing fitness
+        print chromosome_list[-1][0]
         new_chromosome_list.append(chromosome_list[-1])
 
         # Crossover n times
@@ -207,23 +328,26 @@ def ga(n, chord_progression, num_iter):
             (child1_genotype, child2_genotype) = crossover(parent1, parent2, d)
 
             # calculate fitness of both children, and add higher to chromosomes
-            fitness1 = calcfitness(child1_genotype)
-            fitness2 = calcfitness(child2_genotype)
+            fitness1 = calc_fitness(child1_genotype)
+            fitness2 = calc_fitness(child2_genotype)
 
             if fitness1 > fitness2:
-                new_chromosome_list.append((fitness1, child1))
+                new_chromosome_list.append((fitness1, child1_genotype))
             else:
-                new_chromosome_list.append((fitness2, child2))
+                new_chromosome_list.append((fitness2, child2_genotype))
 
 
         # Mutate based on certain probabilities
         # decide on hill-climbing (don't replace parent if it was not at
         # least as fit!)
-        for chrom in new_chromosome_list:
-            mutate(chrom)
+        for i, chrom in enumerate(new_chromosome_list):
+            if i == 0:  # maintain elitism
+                continue
+            new_genotype = mutate(chrom, d, prob_local=1)
+            new_fitness = calc_fitness(new_genotype)
+            new_chromosome_list[i] = (new_fitness, new_genotype)
 
-
-        # OPERATE (mate)
+        chromosome_list = new_chromosome_list
 
 
 def main():
