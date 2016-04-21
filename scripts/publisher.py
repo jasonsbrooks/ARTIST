5c78db72d9a4b76f6dd1234595a27d59a456c003
Jason Brooks <jason.brooks@yale.edu>
2016-04-21 17:21:54 -0400

    Sleeps added back in rather than wait_for

--- scripts/publisher.py
+++ scripts/publisher.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import json, copy, rospy
+import json, copy, rospy, pdb
 
 from artist_performer import Performer
 import baxter_artist.msg
@@ -19,7 +19,7 @@ class NotePublisher(object):
                 
         # construct the note message
         msg = baxter_artist.msg.Note()
-        msg.starttime = rospy.Time.now() + rospy.Duration.from_sec(5)
+        msg.starttime = rospy.Time.now() + rospy.Duration.from_sec(1)
         msg.pitch = note 
 
         rospy.loginfo("publish - pitch: " + str(msg.pitch))
@@ -48,10 +48,13 @@ def main():
         "A6":73,
         "B6":75,
         "C7":76}
+    # pdb.set_trace()
 
     # noteNameArray = ["C6", "E6", "G6", "C7", "D6", "B6", "C7", "C6", "C6", "C6", "E6", "G6", "C7", "D6", "B6", "C7", "C6", "C6", "A6", "G6", "F6", "E6", "D6", "G6", "F6", "E6", "D6", "C6", "B5", "C6", "D6", "B5", "D6"]
-    noteNameArray = ["C6", "D6", "E6", "F6", "G6", "A6", "B6", "C7"]
-    # noteNameArray = ["C7", "B5", "C7","B5", "C7","B5", "C7","B5", "C7"]
+    # noteNameArray = ["C6", "D6", "E6", "F6", "G6", "A6", "B6", "C7"]
+    # noteNameArray = ["B5", "C6", "D6", "C6", "B5", "C6", "D6", "C6", "B5", "C6", "D6", "C6"]
+    # noteNameArray = ["B5", "C7", "B5", "C7", "B5", "C7", "B5", "C7", "B5", "C7"]
+    noteNameArray = ["C7", "B5", "C7","B5", "C7","B5", "C7","B5", "C7"]
     # noteNameArray = ["C7", "E6"]
     noteValArray = [notes[x] for x in noteNameArray]
 
