ca25cd57a80d629f2c54ea4b8210db023de9222b
Jason Brooks <jason.brooks@yale.edu>
2016-04-21 00:28:21 -0400

    Refactored code to support multiple arms

--- src/baxter_artist/performer.py
+++ src/baxter_artist/performer.py
@@ -45,14 +45,17 @@ class Performer(BaxterController):
         with open(KEYS_FILENAME) as f:
             self.keys = json.load(f)
 
+        self.right_notes = [int(x) for x in keys['right'].keys()]
+        self.left_notes = [int(x) for x in keys['left'].keys()]
+
     def flick(self,arm,current_pos):
         down_pos = copy.deepcopy(current_pos)
         down_pos[arm + "_w1"] = current_pos[arm + "_w1"] + 0.08
 
         arm_obj = (self.left_arm if arm == "left" else self.right_arm)
 
-        wait_for(lambda: self.checkRightJointPositions(down_pos), rate=4, timeout=2.0, body=arm_obj.set_joint_positions(down_pos))
-        wait_for(lambda: self.checkRightJointPositions(current_pos), rate=4, timeout=2.0, body=arm_obj.set_joint_positions(current_pos))
+        wait_for(lambda: self.checkJointPositions(down_pos, arm), rate=4, timeout=2.0, body=arm_obj.set_joint_positions(down_pos))
+        wait_for(lambda: self.checkJointPositions(current_pos, arm), rate=4, timeout=2.0, body=arm_obj.set_joint_positions(current_pos))
 
 
     def get_joint_angles(self):
@@ -72,10 +75,16 @@ class Performer(BaxterController):
         pub = rospy.Publisher('/robot/xdisplay', Image, latch=True, queue_size=1)
         pub.publish(msg)
 
-    def play_note_now(self,note):
+    def play_right_note_now(self,note):
         self.send_note(int(note))
-        wait_for(lambda: self.checkRightJointPositions(self.keys["right"][str(note)]), rate=4, raise_on_error=False, timeout=2.0, body=self.right_arm.set_joint_positions(self.keys["right"][str(note)], raw=True))
-        wait_for(lambda: self.checkRightJointPositions(self.keys["right"][str(note)]), rate=4, raise_on_error=False, timeout=2.0, body=self.flick("right", self.keys["right"][str(note)]))
+        wait_for(lambda: self.checkJointPositions(self.keys["right"][str(note)], "right"), rate=4, raise_on_error=False, timeout=2.0, body=self.right_arm.set_joint_positions(self.keys["right"][str(note)], raw=True))
+        wait_for(lambda: self.checkJointPositions(self.keys["right"][str(note)], "right"), rate=4, raise_on_error=False, timeout=2.0, body=self.flick("right", self.keys["right"][str(note)]))
+
+    def play_left_note_now(self, note):
+        self.send_note(int(note))
+        wait_for(lambda: self.checkJointPositions(self.keys["left"][str(note)], "left"), rate=4, raise_on_error=False, timeout=2.0, body=self.left_arm.set_joint_positions(self.keys["left"][str(note)], raw=True))
+        wait_for(lambda: self.checkJointPositions(self.keys["left"][str(note)], "left"), rate=4, raise_on_error=False, timeout=2.0, body=self.flick("left", self.keys["left"][str(note)]))
+
 
     def play_note(self,note):
         sleep_time = note.starttime - rospy.Time.now()
@@ -88,7 +97,12 @@ class Performer(BaxterController):
         rospy.sleep(sleep_time)
 
         rospy.loginfo("playing: " + str(note.pitch))
-        self.play_note_now(note.pitch)
+        if note.pitch in self.right_notes:
+            self.play_right_note_now(note.pitch)
+        elif note.pitch in self.left_notes:
+            self.play_left_note_now(note.pitch)
+        else:
+            rospy.loginfo("Note with no corresponding key tried to play. Check yo_self.")
 
     def subscribe(self):
         subscriber = NoteSubscriber(self.q)
@@ -103,9 +117,12 @@ class Performer(BaxterController):
 
         subscriber.join()
 
-    def checkRightJointPositions(self, target_pos):
-        # print "CHECKING"
-        current_joint_positions = self.right_arm.joint_angles()
+    def checkJointPositions(self, target_pos, arm):
+        current_joint_positions = self.left_arm.joint_angles()
+
+        # update if curious about the other arm
+        if arm == 'right':
+            current_joint_positions = self.right_arm.joint_angles()
         for key in target_pos:
             difference = abs(target_pos[key] - current_joint_positions[key])
             # print "checked %s with difference %f" %(key, difference)
