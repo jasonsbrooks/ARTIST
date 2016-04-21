ca547ee6e1ea7aeaca68d5ffa524fe3de3f23dd6
Charlie Proctor <charlie@charlieproctor.com>
2016-04-21 18:18:55 -0400

    increase pool_size, max_overflow

--- non_baxter/db/__init__.py
+++ non_baxter/db/__init__.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql://charlierproctor@localhost:5432/artist")
+engine = create_engine("postgresql://charlierproctor@localhost:5432/artist", pool_size=20, max_overflow=100)
 Session = sessionmaker(bind=engine)
 
 from sqlalchemy.ext.declarative import declarative_base
