fc95513fff1a585f75710296a59ac848fc6100a2
Ubuntu <ubuntu@ip-172-31-30-240.ec2.internal>
2016-04-22 06:36:07 +0000

    debugging trainer using various directories!

--- non_baxter/ngram/train.py
+++ non_baxter/ngram/train.py
@@ -30,9 +30,9 @@ from ngram_helper import key_transpose_pitch
 NUM_NOTES = 128
 
 class RomanTrainer(object):
-    def __init__(self,proc,name,counts,options):
-        self.proc = proc
-        self.name = name
+    def __init__(self,p_id,rt_id,counts,options):
+        self.p_id = p_id
+        self.rt_id = rt_id
 
         self.counts = counts
         self.triple = deque()
@@ -46,8 +46,10 @@ class RomanTrainer(object):
         notes = list(self.triple)
         for note in notes:
             src_key = (note.track.key_sig_top,note.track.key_sig_bottom)
-            res.append(key_transpose_pitch(note.pitch,src_key,self.dest_key))
-        return res
+            # TODO: actually transpose the pitch
+            #res.append(key_transpose_pitch(note.pitch,src_key,self.dest_key))
+            res.append(note.pitch)
+	return res
 
     def train(self,note):
         self.triple.append(note)
@@ -63,11 +65,11 @@ class RomanTrainer(object):
 
     # write the results
     def write(self):
-        with open(os.path.join(self.options.outdir,str(self.proc),str(self.name) + ".npy"), 'w') as outfile:
+       with open(os.path.join(self.options.outdir,str(self.p_id),str(self.rt_id) + ".npy"), 'w') as outfile:
             np.save(outfile, self.counts)
 
 class TrackTrainer(Process):
-    def __init__(self,name,session,options):
+    def __init__(self,p_id,session,options):
         Process.__init__(self)
         self.session = session
         self.options = options
@@ -77,18 +79,13 @@ class TrackTrainer(Process):
 
         # construct the roman trainers
         for i in xrange(7):
-            rt = RomanTrainer(name,i + 1,np.zeros(matrix_size, dtype=np.int16),options)
+            rt = RomanTrainer(p_id,i + 1,np.zeros(matrix_size, dtype=np.int16),options)
             self.rts.append(rt)
 
     def run(self):
         # iterate through all the tracks
         for trk in self.session.query(Track).all():
             self.train(trk)
-
-        # make the process output directory if not there already
-        pt = os.path.join(self.options.outdir,str(self.name))
-        if not os.path.exists(pt):
-            os.mkdir(pt)
 
         # write all the rts
         for rt in self.rts:
@@ -129,6 +126,14 @@ def main():
 
     (options, args) = parser.parse_args()
 
+    # make the process output directory if not there already
+    for p_id in xrange(options.pool_size):
+	print options.outdir
+    	pt = os.path.join(options.outdir,str(p_id) + "/")
+	print pt
+        if not os.path.exists(pt):
+            os.mkdir(pt)
+
     sessions = get_sessions(options.pool_size,options.db_username,options.db_password)
     processes = []
 
@@ -151,12 +156,12 @@ def main():
 
     for p_id in xrange(options.pool_size):
         for rt_id in xrange(7):
-            with open(os.path.join(options.outdir,str(p_id),str(rt_id) + ".npy")) as f:
+            with open(os.path.join(options.outdir,str(p_id),str(rt_id + 1) + ".npy")) as f:
                 counts = np.load(f)
                 cumulative_counts[rt_id] = np.add(cumulative_counts[rt_id],counts)
 
     for i in xrange(7):
-        with open(os.path.join(options.outdir,str(i+1),".npy"), "w") as f:
+        with open(os.path.join(options.outdir + "/",str(i+1) + ".npy"), "w") as f:
             np.save(f,cumulative_counts[i])
 
 if __name__ == '__main__':
