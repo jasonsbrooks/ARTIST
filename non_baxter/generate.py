0b2ee8c52e81f4820b59158f5372441485634d0a
Charlie Proctor <charlie@charlieproctor.com>
2016-04-22 08:09:34 +0000

    fixing optparse

--- non_baxter/generate.py
+++ non_baxter/generate.py
@@ -6,21 +6,21 @@ TWELVE_BAR_BLUES = [1,1,1,1,4,4,1,1,5,4,1,1]
 def main():
     parser = OptionParser()
 
-    parser.add_option("-n", "--ngram", dest="ngram")
-    parser.add_option("-g", "--ga", dest="ga", type="int", default=800)
+    parser.add_option("-n", "--ngram", dest="ngram",default="undefined")
+    parser.add_option("-g", "--ga", dest="ga", type="int", default=-1)
 
     (options, args) = parser.parse_args()
 
     ngram_output = []
-    if not (options.__dict__["ngram"] is None):
+    if options.ngram != "undefined":
         ngram_output = ngram.generate(options.ngram,TWELVE_BAR_BLUES)
         print ngram_output
 
-    if not (options.__dict__["ga"] is None):
+    if options.ga != -1:
         ga.run(ngram_output=ngram_output,num_iter=options.ga)
     else:
         # just output the ngram midi
-        ga.create_midi_file(ngram_output, TWELVE_BAR_BLUES)
+        ga.create_midi_file((0,ngram_output), TWELVE_BAR_BLUES)
 
 if __name__ == '__main__':
     main()
