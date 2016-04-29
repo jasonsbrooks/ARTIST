93b9dd242ef6b932aa42565e0048052c5d9e23a4
Jason Brooks <jason.brooks@yale.edu>
2016-04-29 16:24:47 -0400

    Added main website

---
+++ js/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function() { 
+  var scroll_start = 0;
+  $('#logo').hide();
+  var offset = $(window).height() / 3;
+  $(document).scroll(function() { 
+     scroll_start = $(this).scrollTop();
+     if(scroll_start > offset) {
+         $('.navbar').addClass('scrolled');
+         $('#logo').show();
+         $('#large-logo').fadeOut();
+      } else {
+         $('.navbar').removeClass('scrolled');
+         $('#logo').hide();
+         $('#large-logo').fadeIn();
+      }
+  });
+
+  $(document).on('click','.scrollToDiv', function(event) {
+      event.preventDefault();
+      var target = "#" + this.getAttribute('data-target');
+      $('html, body').animate({
+          scrollTop: $(target).offset().top - 50
+      }, 1000);
+  });
+
+});
